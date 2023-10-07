6e7e42412e5a0fa1a8b9304a5e85e89f7c2eabe3
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2023-10-07 14:48:43 -0500

    ln

--- install.zsh
+++ install.zsh
@@ -1,12 +1,4 @@
 #!/bin/zsh
 
-ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
-ln -s -f ~/dotfiles/.zshrc ~/.zshrc
-ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
-ln -s -f ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s -f ~/dotfiles/.p10k.zsh ~/.p10k.zsh
-
-cp -r -f ~/dotfiles/powerlevel10k/ ~/
-
 chsh -s $(which zsh)
 chmod +x ~/dotfiles/install.zsh
---
+++ link.zsh
@@ -0,0 +1,9 @@
+# git
+ln -s -f ~/dotfiles/.gitconfig ~/.gitconfig
+# tmux
+ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
+# zsh
+ln -s -f ~/dotfiles/.zshrc ~/.zshrc
+ln -s -f ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
+ln -s -f ~/dotfiles/.p10k.zsh ~/.p10k.zsh
+ln -s -f ~/dotfiles/powerlevel10k/ ~/
