9b973cd7bda4b4d9d1f26c2550f73ed9028a1ce8
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2024-08-02 18:11:24 -0500

    Add Zsh autosuggestions install and p10k dracula install to install script

--- install.zsh
+++ install.zsh
@@ -4,3 +4,12 @@ chsh -s $(which zsh)
 chmod +x ~/dotfiles/install.zsh
 # C-B + I
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+brew install zsh-autosuggestions
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+git clone https://github.com/dracula/powerlevel10k.git
+cd powerlevel10k
+cp ./files/.p10k.zsh ~/.p10k.zsh
+cd ..
+rm -rf powerlevel10k
