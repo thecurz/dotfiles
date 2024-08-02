3015958dfff480244ae5a69aa3dd0ef9b8a6f2e7
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2024-08-02 18:12:21 -0500

    Removed default .p10k.zsh

--- link.zsh
+++ link.zsh
@@ -7,6 +7,5 @@ ln -s -f ~/dotfiles/.tmux.conf ~/.tmux.conf
 # zsh
 ln -s -f ~/dotfiles/.zshrc ~/.zshrc
 ln -s -f ~/dotfiles/.oh-my-zsh ~/.oh-my-zsh
-ln -s -f ~/dotfiles/.p10k.zsh ~/.p10k.zsh
 ln -s -f ~/dotfiles/powerlevel10k/ ~/
 ln -s -f ~/dotfiles/.clang-format ~/.clang-format
