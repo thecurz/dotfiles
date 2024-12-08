9a3a6694c64ea8ffe0dfc1b856250decf0e2b44c
Carlos Castilla <63982489+thecurz@users.noreply.github.com>
2024-12-08 17:49:21 -0500

    ...

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -106,63 +106,6 @@ M.setup = function()
 	local capabilities = cmp_nvim_lsp.default_capabilities()
 	local util = require("lspconfig.util")
 	local servers = { "html", "cssls", "clangd" }
-	--for _, lsp in ipairs(servers) do
-	--lspconfig[lsp].setup({
-	--	on_attach = on_attach,
-	--		capabilities = capabilities,
-	--})
-	--end
-	--lspconfig.tailwindcss.setup({
-	--on_attach = on_attach,
-	--capabilities = capabilities,
-	--})
-	--lspconfig.gopls.setup({
-	--on_attach = on_attach,
-	--capabilities = capabilities,
-	--cmd = { "gopls" },
-	--filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	--root_dir = util.root_pattern("go.work", "go.mod", ".git"),
-	--settings = {
-	--gopls = {
-	--completeUnimported = true,
-	--usePlaceholders = true,
-	--analyses = {
-	--unusedparams = true,
-	---- unreachable = false,
-	--},
-	--},
-	--},
-	--})
-	--lspconfig.jdtls.setup({
-	--on_attach = on_attach,
-	--capabilities = capabilities,
-	--cmd = { "jdtls" },
-	--filetypes = { "java" },
-	--root_dir = util.root_pattern("pom.xml", ".git"),
-	--})
-	--lspconfig.clangd.setup({
-	--on_attach = function(client, bufnr)
-	--client.server_capabilities.signatureHelpProvider = false
-	--on_attach(client, bufnr)
-	--end,
-	--capabilities = capabilities,
-	--})
-	--lspconfig.pyright.setup({
-	--on_attach = on_attach,
-	--capabilities = capabilities,
-	--filetypes = { "python" },
-	--})
-	--lspconfig.tsserver.setup({
-	--on_attach = on_attach,
-	--capabilities = capabilities,
-	--init_options = {
-	--preferences = {
-	--disableSuggestions = true,
-	--},
-	--},
-	---- filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-	---- cmd = { "typescript-language-server", "--stdio" }
-	--})
 
 	mason_lspconfig.setup_handlers({
 		function(server_name)
